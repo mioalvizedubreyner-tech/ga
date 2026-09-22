03a65645bf90267bb82d5a9c59c496835a0baa8d
mioalvizedubreyner-tech <mioalvizedubreyner@gmail.com>
2026-09-21 19:24:59 -0500

    Create script.js

---
+++ flores/script.js
@@ -0,0 +1,8 @@
+function mostrarSorpresa() {
+
+    const mensaje = document.getElementById("sorpresa");
+
+    mensaje.textContent =
+        "🌻 Que tu vida siempre esté llena de luz, alegría y mucho cariño. ¡Te quiero mucho! 💛  ahh si me costo mucho hacer esto  minimoo 5 mentiritas para mi";
+
+}
